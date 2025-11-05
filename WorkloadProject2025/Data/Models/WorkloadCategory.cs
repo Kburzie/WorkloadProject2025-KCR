207c93dd8c647402738e83d2a4cb85c603a8f9e6
CaraCrowley <Cian.Crowley@mymhc.ca>
2025-11-05 14:58:49 -0700

    Adjusted workloadcategory model

--- WorkloadProject2025/Data/Models/WorkloadCategory.cs
+++ WorkloadProject2025/Data/Models/WorkloadCategory.cs
@@ -5,8 +5,6 @@ namespace WorkloadProject2025.Data.Models
     public class WorkloadCategory
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
         public int MinimumHours { get; set; }
         public int MaximumHours { get; set; }
         public DateTime? StartDate { get; set; }
