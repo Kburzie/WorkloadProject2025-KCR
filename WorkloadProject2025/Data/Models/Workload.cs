16fd7c91a5eee9eafd66a75783a5eb5f67a00e71
CaraCrowley <Cian.Crowley@mymhc.ca>
2025-11-06 16:26:21 -0700

    Creating workload model

---
+++ WorkloadProject2025/Data/Models/Workload.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkloadProject2025.Data.Models;
+
+public class Workload
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "You must specify a faculty member")]
+    public string? FacultyEmail { get; set; }
+    public Faculty Faculty { get; set; }
+
+    [Required(ErrorMessage = "You must specify a workload type")]
+    public int? WorkloadTypeId { get; set; }
+    public WorkloadType WorkloadType { get; set; }
+
+    [Required(ErrorMessage = "You must specify a workload term")]
+    public int? TermId { get; set; }
+    public Term Term { get; set; }
+
+    public double Hours { get; set; }
+
+    //Not required in the case of the workload not being coursework (Ex. coordinator or research)
+    public int? CourseId { get; set; }
+    public Course Course { get; set; }
+}
